37c6d303b058d51ced06cbc21e5f5339e69fe771
jtskidmore <johntoddskidmore@gmail.com>
2020-08-03 13:41:49 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+
+  end
+
+  get "/name" do
+    "My name is JT"
+
+  end
+
+  get "/hometown" do
+    "My hometown is Dallas"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Rattlesnake"
+  end
+
 end
