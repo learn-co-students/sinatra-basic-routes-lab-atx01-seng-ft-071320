cb5712484a1186aef0867c6729fc17367f59063c
Gabe-Corral <78gabe.corral@gmail.com>
2020-08-03 15:34:11 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do
+		"Hello, World!"
+	end
+
+	get '/name' do
+		"My name is Gabe"
+	end
+
+	get '/hometown' do 
+		"My hometown is Austin, TX"
+	end
+
+	get '/favorite-song' do 
+		"My favorite song is some_song_name"
+	end
 end
